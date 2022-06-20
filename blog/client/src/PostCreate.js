57f35581950a5f4cd6ff08fcc571113859be4ab2
abhi893 <abhishek98.ac@gmail.com>
2022-06-20 12:59:39 +0530

    added post and create routes in backend

---
+++ blog/client/src/PostCreate.js
@@ -0,0 +1,30 @@
+import React, {useState} from "react";
+import axios from 'axios';
+
+
+const App = ()=>{
+    const [title, setTitle] = useState('');
+
+    const onSubmit = async(event) =>{
+        event.preventDefault();
+
+        await axios.post('http://localhost/4000/posts', {
+            title
+        });
+
+        setTitle('');
+    };
+
+    return <div>
+        <form onSubmit={onSubmit}>
+            <div className="form-group">
+            <label>title</label>
+            <input value = {title} onChnage={e=> setTitle(e.target.value)}
+             className="form-control"></input>
+            </div>
+            <button className="btn btn-primary">Submit</button>
+        </form>
+    </div>
+};
+
+export default App;
